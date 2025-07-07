a5d4bf7d02be0fa59b933cf65282d07253226ebc
Edvaldo Lima <edvaldoglima23@gmail.com>
2025-07-07 18:09:13 -0300

    Atualizar configuração do Docker para uso do PostgreSQL, incluindo ajustes no docker-compose.yml e no Dockerfile. Adicionar arquivo requirements.txt com dependências do projeto Django.

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DJANGO_DB_NAME', 'tarefas'),
+        'USER': os.environ.get('DJANGO_DB_USER', 'user'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'senha123'),
+        'HOST': os.environ.get('DJANGO_DB_HOST', 'localhost'),
+        'PORT': '5432',
     }
 }
 
