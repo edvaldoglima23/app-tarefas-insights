5b7a2236bebc3dab6c7f06c292b64924bd2fccd5
Edvaldo Lima <edvaldoglima23@gmail.com>
2025-07-09 20:13:09 -0300

    fix: configurar servir arquivos estáticos em produção

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -27,3 +29,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('tasks.urls')),
 ]
+
+# Servir arquivos estáticos em produção
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
